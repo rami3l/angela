e649ceaf210218d7b0fc41078f409bf1beca9df8
rami3l <rami3l@outlook.com>
2022-01-03 11:05:01 +0100

    feat: add telegram bot skeleton

--- Program.fs
+++ Program.fs
@@ -1,1 +1,36 @@
-﻿printfn "Heroes never die!"
+﻿module Angela.Program
+
+open ExtCore.Control.WorkflowBuilders
+open Funogram.Api
+open Funogram.Telegram.Api
+open Funogram.Telegram.Bot
+
+let onHello (context: UpdateContext) =
+    // printfn $"Received update: {context.Update.UpdateId}"
+    maybe {
+        let! message = context.Update.Message
+        let! name = message.Chat.FirstName
+
+        $"Angela: Hi, {name}!"
+        |> sendMessage message.Chat.Id
+        |> api context.Config
+        |> Async.RunSynchronously
+        |> Result.mapError (fun e -> printfn $"Error: {e}")
+        |> ignore
+    }
+    |> ignore
+
+let onUpdate (context: UpdateContext) =
+    processCommands context [ cmd "/hello" onHello ]
+    |> ignore
+
+let launch (token: string) : Async<unit> =
+    startBot { defaultConfig with Token = token } onUpdate None
+
+[<EntryPoint>]
+let main (_: array<string>) : int =
+    launch "bot-token" // TODO: Change this placeholder
+    |> Async.RunSynchronously
+    |> ignore // TODO: Process API responses somehow
+
+    0
