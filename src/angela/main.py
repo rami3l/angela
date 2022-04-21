98b3144623eb04794845fec1b95bf122d5022e2a
rami3l <rami3l@outlook.com>
2022-04-21 08:37:19 +0200

    feat(bot): add `decide`

--- src/angela/main.py
+++ src/angela/main.py
@@ -1,7 +1,9 @@
 import argparse
 import logging
 import os
+import random
 
+import coloredlogs
 from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 
@@ -13,7 +15,7 @@ def greeting() -> str:
 def main() -> None:
     load_dotenv()
 
-    parser = argparse.ArgumentParser(description="test")
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         "--token",
         default=os.environ.get("ANGELA_TELEGRAM_BOT_TOKEN"),
@@ -31,20 +33,32 @@ def main() -> None:
         4: logging.DEBUG,
     }.get(opts.verbosity, logging.INFO)
     logging.basicConfig(level=verbosity)
+    coloredlogs.install()
 
-    logging.info("Angela is waking up...")
+    logging.warning("Angela is waking up...")
 
     bot = Bot(token=opts.token)
     dp = Dispatcher(bot)
 
     dp.message_handler(commands="hello")(hello)
+    dp.message_handler(commands="decide")(decide)
 
     executor.start_polling(dp)
 
 
 async def hello(msg: types.Message) -> None:
-    title = src.first_name if (src := msg.from_user) is not None else "Hi"
-    await msg.reply(f"{title}, I'm right beside you!")
+    title = (src := msg.from_user) and src.first_name or "Hi"
+    await msg.reply(f"👋 {title}, I'm right beside you!")
+
+
+async def decide(msg: types.Message) -> None:
+    formats = ["🤔 Emmm... I'd say {}.", "💡 What about {}?"]
+    options = msg.text.split()[1:]
+    if not options:
+        title = (src := msg.from_user) and src.first_name or "Hi"
+        await msg.reply(f"🤔 {title}, what's on your mind?")
+        return
+    await msg.reply(random.choice(formats).format(random.choice(options)))
 
 
 if __name__ == "__main__":
