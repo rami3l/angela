682af0edce8419dafb3198a5b560ae342d7a6d1e
rami3l <rami3l@outlook.com>
2025-06-30 23:27:32 +0800

    feat(command): impl `/hello`

---
+++ lib/angela/command.ex
@@ -0,0 +1,20 @@
+defmodule Angela.Command do
+  @moduledoc """
+  The "raw" command handler agnostic to the bot implementation.
+  """
+
+  alias ExGram.Model.Message
+
+  defmodule Response do
+    @moduledoc """
+    The response structure for a "raw" command handler.
+    """
+
+    @type t :: %__MODULE__{txt: String.t(), opts: keyword()}
+    defstruct txt: "", opts: []
+
+    def new(txt, opts \\ []), do: %__MODULE__{txt: txt, opts: opts}
+  end
+
+  @callback respond(msg :: Message.t()) :: Response.t()
+end
---
+++ lib/angela/command/hello.ex
@@ -0,0 +1,18 @@
+defmodule Angela.Command.Hello do
+  @moduledoc """
+  Greets the user.
+  """
+
+  alias Angela.Command.Response
+  alias ExGram.Model.{Message, ReplyParameters, User}
+
+  @behaviour Angela.Command
+
+  @impl true
+  def respond(msg = %Message{from: sender = %User{}}) do
+    name = sender.first_name || "Hi"
+
+    "#{name}, I'm right beside you!"
+    |> Response.new(reply_parameters: %ReplyParameters{message_id: msg.message_id})
+  end
+end
