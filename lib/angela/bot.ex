c3883a900b5db7876193e59d36c9deebe31bf10d
rami3l <rami3l@outlook.com>
2025-06-30 23:27:32 +0800

    feat(command): impl `/hello`

--- lib/angela/bot.ex
+++ lib/angela/bot.ex
@@ -1,22 +1,28 @@
 defmodule Angela.Bot do
+  @moduledoc """
+  The bot's core properties and the registration of commands it can handle.
+  """
+
+  alias ExGram.{Cnt, Model.Message}
+  alias Angela.Command.{Hello, Response}
+
   @bot :angela
 
   use ExGram.Bot,
     name: @bot,
     setup_commands: true
 
-  command("start")
-  command("help", description: "Print the bot's help")
-
   middleware(ExGram.Middleware.IgnoreUsername)
 
   def bot(), do: @bot
 
-  def handle({:command, :start, _msg}, cx) do
-    cx |> answer("Hi!")
+  @spec reply(module(), Cnt.t(), Message.t()) :: Cnt.t()
+  def reply(cmd, cx, msg) do
+    resp = %Response{} = cmd.respond(msg)
+    answer(cx, resp.txt, resp.opts)
   end
 
-  def handle({:command, :help, _msg}, cx) do
-    cx |> answer("Here is your help:")
-  end
+  command("hello", description: "👋")
+  @impl ExGram.Handler
+  def handle({:command, :hello, msg}, cx), do: Hello |> reply(cx, msg)
 end
