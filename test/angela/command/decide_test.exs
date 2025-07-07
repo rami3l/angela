860db864aca8e1004a6801136053e3e5c937e518
rami3l <rami3l@outlook.com>
2025-07-07 13:11:17 +0800

    refactor(test): clean up test cases

--- test/angela/command/decide_test.exs
+++ test/angela/command/decide_test.exs
@@ -3,35 +3,37 @@ defmodule Angela.Command.DecideTest do
   doctest Angela.Command.Decide
 
   alias Angela.Command.Decide
-  alias ExGram.Model.{Message, ReplyParameters}
+  alias ExGram.Model.Message
 
   import AssertMatch
 
   describe "respond/1" do
+    @respond &Decide.respond/1
+    @msg_id %{message_id: 2048}
+
+    defp msg(params), do: struct!(Message, params |> Map.merge(@msg_id))
+
     test "responds with the only choice" do
-      %Message{text: " rice ", message_id: 123}
-      |> Decide.respond()
+      msg(%{text: " rice "})
+      |> @respond.()
       |> assert_match(%{
         txt: "rice",
-        opts: [reply_parameters: %ReplyParameters{message_id: 123}]
+        opts: [reply_parameters: @msg_id]
       })
     end
 
     test "responds with one of the provided choices" do
-      %Message{text: "pizza pasta burger", message_id: 456}
-      |> Decide.respond()
+      msg(%{text: "pizza pasta burger"})
+      |> @respond.()
       |> assert_match(
-        %{
-          txt: choice,
-          opts: [reply_parameters: %ReplyParameters{message_id: 456}]
-        }
+        %{txt: choice, opts: [reply_parameters: @msg_id]}
         when choice in ["pizza", "pasta", "burger"]
       )
     end
 
     test "throws `MatchError` when no choices are provided" do
-      MatchError
-      |> assert_raise(fn -> Decide.respond(%Message{text: "", message_id: 789}) end)
+      fn -> msg(%{text: ""}) |> @respond.() end
+      |> then(&assert_raise(MatchError, &1))
     end
   end
 end
