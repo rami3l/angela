219a3173cca849d1aba34a0a53e4f3e8af1f0775
rami3l <rami3l@outlook.com>
2025-07-16 16:47:45 +0800

    refactor(config): migrate some configs to runtime with `dotenvy`

--- config/dev.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-config :angela, setup_commands: true
-
-import_config "dev.secret.exs"
--- config/prod.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-config :angela, setup_commands: true
-
-import_config "prod.secret.exs"
