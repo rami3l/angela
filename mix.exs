e308ca49e977f87d83844b8fd6dc3f868eb983a9
rami3l <rami3l@outlook.com>
2025-10-16 21:01:43 +0800

    chore(deps): get `credo` from Hex instead

--- mix.exs
+++ mix.exs
@@ -30,8 +30,7 @@ defmodule Angela.MixProject do
 
       # Dev dependencies
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
-      # {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:credo, github: "rrrene/credo", only: [:dev, :test], runtime: false, override: true},
+      {:credo, "~> 1.7.13", only: [:dev, :test], runtime: false},
       {:assert_match, "~> 1.0", only: [:test]},
       {:mox, "~> 1.0", only: [:test]}
     ]
