27934c03949f918abc800ebbf3ec605e24de5fe0
rami3l <rami3l@outlook.com>
2025-10-16 20:54:00 +0800

    fix(command/etymology): add user agent to HTTP client

--- test/angela_test.exs
+++ test/angela_test.exs
@@ -2,7 +2,9 @@ defmodule AngelaTest do
   use ExUnit.Case
   doctest Angela
 
-  test "greets the world" do
-    assert Angela.hello() == :world
+  import AssertMatch
+
+  test "has a version string" do
+    Version.parse(Angela.version()) |> assert_match({:ok, _})
   end
 end
